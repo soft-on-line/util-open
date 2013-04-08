94fd9e4638b3e3e2bb6ed6061e84b30fd0b305ee
soft-on-line <zhima_2008@163.com>
2013-04-08 00:38:30 -0700

    util-open:Test类删除

--- util-open/src/main/java/org/open/util/debug/Test.java
+++
@@ -1,8 +0,0 @@
-package org.open.util.debug;
-
-import org.open.util.debug.DebugUtil;
-
-public class Test 
-{
-	protected static DebugUtil du = new DebugUtil(DebugUtil.InstanceModel.ConsoleModel); 
-}
